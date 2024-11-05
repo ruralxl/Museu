057d7cfedd8a7cfc2dda22829e0640807dcd72d4
leoleozao <103790766+leoleozao@users.noreply.github.com>
2024-11-05 11:52:58 -0300

    nav

--- js/nav.js
+++ js/nav.js
@@ -7,18 +7,18 @@ $(function(){
     })
 
     function verificarCliqueFechar(){
-        $('html,body').click(()=>{
-            $('.menu-mobile').animate({'width':'0'});
-            if($('menu-mobile').css('width') == 0){
+        $('html,body').click(() => {
+            $('.menu-mobile').animate({'width':'0'}, 300, function() {
                 $('.menu-mobile').css('display','none');
-            }
+                if (parseInt($('html,body').css('width')) <= 1060) {
+                    $('.three-points').css('display','block');
+                }
+            });
+        });
 
-            $('.three-points').css('display','block');
-        })
-
-        $('.menu-mobile').click((e)=>{
+        $('.menu-mobile').click((e) => {
             e.stopPropagation();
-        })
+        });
     }
 
     verificarCliqueFechar();
